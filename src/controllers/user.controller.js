e54418cde1a933b6dec76989c336f65035d98127
Ankush Kumar <ankushkumar9618@gmail.com>
2025-03-16 00:01:57 +0530

    Added user controller and registered First User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,61 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "OK"
-    })
+    const { userName, fullName, email, password } = req.body;
+
+    if ([userName, email, fullName, password].some((item) => item?.trim() === "")) {
+        throw new ApiError(400, "All The Fields are Required to Proceed..");
+        //res.status(401).json(response);
+    }
+
+    const userExists = await User.findOne({
+        $or: [{ userName }, { email }]
+    });
+    if (userExists) {
+        throw new ApiError(409, "UserName or Email already Exits..");
+    }
+
+    const avatarFilePath = req.files?.avatar[0]?.path;
+    const coverImagePath = req.files?.coverImage[0]?.path;
+
+    if (!avatarFilePath) {
+        throw new ApiError(400, "Avatar is Required to Proceed..")
+    }
+
+    const avatar = await uploadToCloudinary(avatarFilePath);
+    const coverImage = await uploadToCloudinary(coverImagePath);
+
+    if (!avatar) {
+        throw new ApiError("505", "Failed Uploading Image to Cloudinary..");
+    }
+
+    const user = await User.create(
+        {
+            userName: userName.toLowerCase(),
+            email,
+            fullName,
+            password,
+            avatar: avatar.url,
+            coverImage: coverImage?.url || ""
+        }
+    );
+
+    const createdUser = User.findById(user._id).select("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(504, "Error registering user in Database");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, "User Registered Successfully..")
+    )
+
+
+
 })
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,7 +45,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
@@ -81,4 +81,4 @@ userSchema.methods.generateRefreshToken = async function () {
 
 
 
-export const user = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
